88959a3aa488b6a4b0d356319b2c2c7dd7e48a81
TrinhBaoDuy <2051050075duy@ou.edu.vn>
2023-10-19 22:00:53 +0700

    update chat thanh cong

--- clinic/src/mychat/ChatBox.js
+++ clinic/src/mychat/ChatBox.js
@@ -1,26 +1,30 @@
 import Message from "./Message";
 import { collection, query, onSnapshot, orderBy, limit, doc } from "firebase/firestore";
-import { useEffect, useRef, useState, useParams } from "react";
+import { useEffect, useRef, useState, useParams, useContext } from "react";
 import { db } from "../firebase";
 import { UserAuth } from "./context/AuthContext";
+import { MyUserContext } from "../App";
 
 
-const ChatBox = () => {
-    // const { username } = useParams();
+const ChatBox = (props) => {
+    const { rep } = props;
     const messagesEndRef = useRef();
     const [messages, setMassages] = useState([]);
     const scrollToBottom = () => {
         messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
     };
+    const [user] = useContext(MyUserContext);
+    // const doituongchat = rep
+    // const url = user.username+"-"+doituongchat;
     // const { currentUser } = UserAuth();
     useEffect(scrollToBottom, [messages])
-
+    const url = "ClinnitChat";
     useEffect(() => {
         // const url_collection = "chat/" + currentUser.name
         // truy vấn (q) để truy xuất dữ liệu từ cơ sở dữ liệu Firestore của Firebase.
         const q = query(
             // db name in firebase
-            collection(db, "newchat",),
+            collection(db, url,),
             orderBy("createdAt"),//sắp xếp theo thời gian 
             limit(50),
         );
@@ -45,10 +49,16 @@ const ChatBox = () => {
     return (
         <div className="pb-44 pt-20 containerWrap">
 
-            {messages.map((message) => (
+            {/* {messages.map((message) => (
 
                 <Message key={message.id} message={message} />
-            ))}
+            ))} */}
+            {messages.map((message) => {
+            if ((message.rep === rep && message.username === user.username) || (message.rep === user.username && message.username === rep)) { 
+                return <Message key={message.id} message={message} rep = {rep} />;
+            }
+            return null;
+            })}
             <div ref={messagesEndRef}></div>
         </div>
     );
--- clinic/src/mychat/pages/ChatRoom.js
+++ clinic/src/mychat/pages/ChatRoom.js
@@ -1,13 +1,15 @@
+import { useParams } from "react-router-dom";
 import ChatBox from "../ChatBox"
 import SendMessage from "../SendMessage"
-
 const ChatRoom = () => {
+    const { rep } = useParams();
+    console.log(rep)
     return (
         <>
             chat room
             < div >
-                <ChatBox />
-                <SendMessage />
+                <ChatBox rep={rep}/>
+                <SendMessage rep={rep}/>
             </ div>
         </>
 
